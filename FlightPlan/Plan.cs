37ff30702525fdb8a816784ccce600676d1ba0bc
cedri <cedri@URSIDAE>
2017-05-11 12:49:39 -0700

    More UML progress

--- FlightPlan/Plan.cs
+++ FlightPlan/Plan.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,18 +7,34 @@ using System.Threading.Tasks;
 
 namespace CIOSDigital.FlightPlan
 {
-    public class Plan
+    public class Plan : IEnumerable<Coordinate>
     {
         private readonly List<Coordinate> Waypoints;
-        
+
         private Plan()
         {
             Waypoints = new List<Coordinate>();
         }
 
-        public Plan Empty()
+        public static Plan Empty()
         {
             return new Plan();
+        }
+
+        public void AppendWaypoint(Coordinate c)
+        {
+            Console.WriteLine("Added waypoint at {0}, {1}", c.Latitude, c.Longitude);
+            this.Waypoints.Add(c);
+        }
+
+        public IEnumerator<Coordinate> GetEnumerator()
+        {
+            return ((IEnumerable<Coordinate>)Waypoints).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Coordinate>)Waypoints).GetEnumerator();
         }
     }
 }
---
+++ MapDB/SqlLiteMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CIOSDigital.MapDB
+{
+    public class SqlLiteMap : MapProvider
+    {
+        public ImageSource GetImage(MapImageSpec spec)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
